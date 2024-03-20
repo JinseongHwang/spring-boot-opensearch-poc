d744075a2bf3797895520bcada4650b0586f3a4f
JinseongHwang <jinseong.dev@gmail.com>
2024-03-20 10:29:33 +0900

    feat: init project

---
+++ src/main/kotlin/me/study/opensearch/SpringBootOpensearchPocApplication.kt
@@ -0,0 +1,11 @@
+package me.study.opensearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootOpensearchPocApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBootOpensearchPocApplication>(*args)
+}
---
+++ src/test/kotlin/me/study/opensearch/SpringBootOpensearchPocApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.study.opensearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootOpensearchPocApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
