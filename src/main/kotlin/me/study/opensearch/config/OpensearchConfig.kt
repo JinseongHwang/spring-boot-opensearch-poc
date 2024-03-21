4aa5878d7d636e0610bf0df37912e7cc69dec333
JinseongHwang <jinseong.dev@gmail.com>
2024-03-21 11:02:21 +0900

    feat: create opensearch client

--- src/main/kotlin/me/study/opensearch/config/OpensearchConfig.kt
+++ src/main/kotlin/me/study/opensearch/config/OpensearchConfig.kt
@@ -1,42 +1,34 @@
 package me.study.opensearch.config
 
+import me.study.opensearch.config.properties.OpensearchProperties
 import org.apache.http.HttpHost
 import org.apache.http.auth.AuthScope
 import org.apache.http.auth.UsernamePasswordCredentials
 import org.apache.http.impl.client.BasicCredentialsProvider
 import org.opensearch.client.RestClient
-import org.opensearch.client.json.jackson.JacksonJsonpMapper
-import org.opensearch.client.opensearch.OpenSearchClient
-import org.opensearch.client.transport.rest_client.RestClientTransport
-import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Bean
+import org.opensearch.client.RestHighLevelClient
+import org.opensearch.data.client.orhlc.AbstractOpenSearchConfiguration
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories
 
 
 @Configuration
-@EnableConfigurationProperties(OpensearchProperty::class)
 @EnableElasticsearchRepositories
 class OpensearchConfig(
-    private val property: OpensearchProperty
-) {
+    private val property: OpensearchProperties,
+) : AbstractOpenSearchConfiguration() {
 
-    // ref: https://opensearch.org/docs/latest/clients/java/
-    @Bean
-    fun opensearchClient(): OpenSearchClient {
+    override fun opensearchClient(): RestHighLevelClient {
+        val credentials = UsernamePasswordCredentials(property.username, property.password)
         val credentialsProvider = BasicCredentialsProvider()
-        credentialsProvider.setCredentials(
-            AuthScope.ANY,
-            UsernamePasswordCredentials(property.username, property.password)
-        )
+        credentialsProvider.setCredentials(AuthScope.ANY, credentials)
 
-        val restClient = RestClient.builder(
-            HttpHost("localhost", 9200, "https")
+        val builder = RestClient.builder(
+            HttpHost.create(property.host)
         ).setHttpClientConfigCallback {
             it.setDefaultCredentialsProvider(credentialsProvider)
-        }.build()
+        }
 
-        val transport = RestClientTransport(restClient, JacksonJsonpMapper())
-        return OpenSearchClient(transport)
+        return RestHighLevelClient(builder)
     }
 }
