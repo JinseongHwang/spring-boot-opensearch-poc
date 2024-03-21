7cceaff15743b1ab2180b2d854ec6a58969ef5aa
JinseongHwang <jinseong.dev@gmail.com>
2024-03-21 11:00:07 +0900

    feat: exclude crash beans

--- src/main/kotlin/me/study/opensearch/SpringBootOpensearchPocApplication.kt
+++ src/main/kotlin/me/study/opensearch/SpringBootOpensearchPocApplication.kt
@@ -2,9 +2,13 @@ package me.study.opensearch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude = [ElasticsearchDataAutoConfiguration::class])
+@SpringBootApplication(exclude = [
+    ElasticsearchDataAutoConfiguration::class,
+    ElasticsearchRestClientAutoConfiguration::class,
+])
 class SpringBootOpensearchPocApplication
 
 fun main(args: Array<String>) {
