2005849636870efb9836938daa0be2380c84fcbd
JinseongHwang <jinseong.dev@gmail.com>
2024-03-21 11:03:02 +0900

    feat: controller to create index

---
+++ src/main/kotlin/me/study/opensearch/controller/OpensearchController.kt
@@ -0,0 +1,44 @@
+package me.study.opensearch.controller
+
+
+import org.opensearch.client.RequestOptions
+import org.opensearch.client.RestHighLevelClient
+import org.opensearch.client.indices.CreateIndexRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/os")
+class OpensearchController(
+    val opensearchClient: RestHighLevelClient
+) {
+
+    @PostMapping("/index")
+    fun createIndex(
+        @RequestParam name: String
+    ): ResponseEntity<String> {
+        if (name.isBlank()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Index name must not be blank.")
+        }
+
+        val createIndexRequest = CreateIndexRequest(name)
+        /*
+        ### Add if you are using replica set
+        createIndexRequest.settings(
+            Settings.builder()
+                .put("index.number_of_shards", 3)
+                .put("index.number_of_replicas", 2)
+        )
+         */
+
+        val indexResponse = opensearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT)
+        println("Index creation response: $indexResponse")
+
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Index=`${name}` created successfully.")
+    }
+}
